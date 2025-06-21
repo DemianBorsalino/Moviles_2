eaed7197f911a5b40c1d6765f76ee2b001ee1248
DemianBorsalino <demianrborsalino@gmail.com>
2025-06-21 04:11:24 -0300

    Funcionalidad terminada

--- app/src/main/java/com/example/parcial_2_moviles/Genero.kt
+++ app/src/main/java/com/example/parcial_2_moviles/Genero.kt
@@ -3,5 +3,5 @@ package com.example.parcial_2_moviles
 import java.io.Serializable
 
 enum class Genero : Serializable {
-    ACCION, COMEDIA, DRAMA, DOCUMENTAL, OTRO
+    ACCION, COMEDIA, DRAMA, DOCUMENTAL, FANTASIA, POLICIAL, TERROR, AVENTURA, SUSPENSO, ROMANCE, MUSICAL, CIENCIA_FICCION
 }
